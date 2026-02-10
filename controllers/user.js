9825849f0001059185f05316c0611b78adff6c1e
Adam Myers <adammyers002@gmail.com>
2026-02-09 21:43:21 -0600

    fix User model to import correctly as an object in user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../models/User");
+const { User } = require("../models/User");
 const verifyToken = require("../middleware/verify-token");
 
 // Index (non-authenticated) GET /users
